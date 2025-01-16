ed888b42128f70653746f9afd39f249dc64e7698
Arivazhahan <114591821+arivu987@users.noreply.github.com>
2025-01-16 22:17:22 +0530

    Add files via upload

---
+++ header-app/src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react'; 
+
+function App() {
+  return (
+    <div className="HeaderApp">
+    <div>Header</div>
+  </div>
+  );
+}
+
+export default App;
---
+++ home-app/src/App.js
@@ -0,0 +1,18 @@
+
+import React, { lazy, Suspense } from 'react';
+
+
+const Header = lazy(() => import('HeaderApp/Header'));
+
+function App() {
+  return (
+    <div className="App">
+    <Suspense fallback={<div>Loading Header...</div>}>
+      <Header />
+    </Suspense>
+    <div className="container">Demo home page</div>
+  </div>
+  );
+}
+
+export default App;
